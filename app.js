f2258ec8afe4996622f5077b7d1325e7e5a4490d
arunk555 <arunkurusev@gmail.com>
2022-11-10 13:35:47 +0000

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express=require("express");
 const app=express();
+const path=require("path");
+const favicon = require('serve-favicon');
 const port=3000;
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get("/welcome", (req,res)=>{
   res.status(200).send("Welcome");
