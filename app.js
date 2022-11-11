38d5506c997f291eefd1794873ba322422042040
arunk555 <arunkurusev@gmail.com>
2022-11-11 10:07:26 +0000

    latest

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.get("/welcome", (req,res)=>{
   res.status(200).send("Welcome");
 });
 
+// This should be the last route else any after it won't work
+app.use("*", (req, res) => {
+    res.status(404).json({
+      success: "false",
+      message: "Page not found",
+      error: {
+        statusCode: 404,
+        message: "You reached a route that is not defined on this server",
+      },
+    });
+  });
+
 app.listen(port,()=>{
 console.log(`Express is running on the port ${port
 }`);
